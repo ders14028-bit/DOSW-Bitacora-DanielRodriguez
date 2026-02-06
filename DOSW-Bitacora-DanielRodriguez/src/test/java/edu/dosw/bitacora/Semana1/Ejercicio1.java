a66e8f3910f1b499bbe703ea3af39d85a92e080c
Daniel Rodriguez <daniel.rsuarez@mail.escuelaing.edu.co>
2026-02-05 19:59:03 -0500

    feature: Ejercicio 1 semana 1 resuelto

---
+++ DOSW-Bitacora-DanielRodriguez/src/test/java/edu/dosw/bitacora/Semana1/Ejercicio1.java
@@ -0,0 +1,27 @@
+package edu.dosw.bitacora.Semana1;
+
+import java.util.List;
+
+//Dada una lista de numeros enteros, necesitamos obtener una nueva lista solo con los numeros pares mayores a diez
+//
+//Datos de Entrada:
+//List<Integer> numbers = List.of(3,8,10,12,15,18,20)
+//Resultado:
+//List<Integer>
+
+
+public class Ejercicio1 {
+    public static void main(String[] args) {
+
+        List<Integer> numbers = List.of(3, 8, 10, 12, 15, 18, 20);
+
+        List<Integer> result = numbers.stream()
+                .filter(n -> n % 2 == 0)
+                .filter(n -> n > 10)
+                .toList();
+
+        System.out.println(result);
+    }
+}
+
+
