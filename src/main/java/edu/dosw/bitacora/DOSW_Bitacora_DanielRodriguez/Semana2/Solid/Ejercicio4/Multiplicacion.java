415aa02491ced9d50f7ed6f34ec74bc98a304f93
Daniel Rodriguez <daniel.rsuarez@mail.escuelaing.edu.co>
2026-02-26 12:14:39 -0500

    ejercicio 4 semana 2 finalizado

---
+++ src/main/java/edu/dosw/bitacora/DOSW_Bitacora_DanielRodriguez/Semana2/Solid/Ejercicio4/Multiplicacion.java
@@ -0,0 +1,10 @@
+package edu.dosw.bitacora.DOSW_Bitacora_DanielRodriguez.Semana2.Solid.Ejercicio4;
+
+public class Multiplicacion implements Operacion {
+
+    @Override
+    public double ejecutar(double a, double b) {
+        return a * b;
+    }
+}
+
---
+++ src/main/java/edu/dosw/bitacora/DOSW_Bitacora_DanielRodriguez/Semana2/Solid/Ejercicio4/Operacion.java
@@ -0,0 +1,7 @@
+package edu.dosw.bitacora.DOSW_Bitacora_DanielRodriguez.Semana2.Solid.Ejercicio4;
+
+public interface Operacion {
+
+    double ejecutar(double a, double b);
+}
+
